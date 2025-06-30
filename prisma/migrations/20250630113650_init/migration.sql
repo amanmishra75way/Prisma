c16791e025b9e8b45252089552590450a220bc62
amanmishra75way <amishra.75way@gmail.com>
2025-06-30 17:10:05 +0530

    npx prisma format

---
+++ prisma/migrations/20250630113650_init/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "UserSubscription" (
+    "id" SERIAL NOT NULL,
+    "isActive" BOOLEAN NOT NULL DEFAULT false,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "UserSubscription_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "UserSubscription_userId_key" ON "UserSubscription"("userId");
+
+-- AddForeignKey
+ALTER TABLE "UserSubscription" ADD CONSTRAINT "UserSubscription_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
